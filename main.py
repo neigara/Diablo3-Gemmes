b9bfb11e483dce10f5d731be24c61c04074244b4
neigara <neigara@gmail.com>
2018-01-30 17:45:02 +0100

    Update main.py
    Reformulation du dernier print

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-stockR = int(input('Nombre de gemmes Royales en stock '))
-stockISD = int(input('Nombre de gemmes Impériales Sans Defaut en stock '))
-stockI = int(input('Nombre de gemmes Impériales en stock '))
-stockM = int(input('Nombre de gemmes Marquises en stock '))
+stockR = int(input('Nombre de gemmes Royales en stock : '))
+stockISD = int(input('Nombre de gemmes Impériales Sans Defaut en stock : '))
+stockI = int(input('Nombre de gemmes Impériales en stock : '))
+stockM = int(input('Nombre de gemmes Marquises en stock : '))
+
 needRSD = int(input('\nNbre de gemmes Royales Sans Défaut voulues ? '))
 
-equiM = (stockM + stockI*3 + stockISD *9 + stockR*27) #equivalent du stock en nombre de gemme marquises
-RSD_possible = equiM//81 #nombre de gemmes Royales Sans Défaut créable avec le stock
-needM = needRSD*81 - equiM #gemmes marquisent manquantes
+#RSD_possible = equiM//81 #nombre de gemmes Royales Sans Défaut créable avec le stock
+equiM = (stockM + stockI*3 + stockISD *9 + stockR*27) #equivalent du stock en nombre de gemme Marquises
+needM = needRSD*81 - equiM #gemmes Marquises manquantes
 
 #print ("Il est possible de faire %d gemmes Royales Sans Défaut" % RSD_possible)
 if (needM < 1):
-    print ("Vous avez suffisement de gemmes Marquises pour faire %d gemmes Royales Sans Défaut" % needRSD)
+    print ("\nVous avez suffisement de gemmes Marquises pour faire %d gemmes Royales Sans Défaut" % needRSD)
 else:
-    print ("\nPour créer %d gemmes Royales Sans Défaut, il faut %d gemmes Marquises supplémentaires" % (needRSD, needM))
+    print ("\nIl vous manque %d gemmes Marquises pour faire %d gemmes Royales Sans Défaut" % (needRSD, needM))
